b1a2b0ffd7220d0afdee956d5f2be80c01607cb9
shuhei <kawashu0723@gmail.com>
2015-06-19 11:32:07 +0900

    Initial Commit

---
+++ iidaPro-2015/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iidaPro-2015
+//
+//  Created by akaimo on 2015/06/19.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iidaPro-2015/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iidaPro-2015
+//
+//  Created by akaimo on 2015/06/19.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

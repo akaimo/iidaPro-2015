56b7727163cfb1fb7f5ea01d85d71b9a81e9e12e
shuhei <kawashu0723@gmail.com>
2015-07-20 14:44:06 +0900

    add searchBar, add backgroundImg and trashImg

--- iidaPro-2015/ViewController.m
+++ iidaPro-2015/ViewController.m
@@ -9,14 +9,17 @@
 #import "ViewController.h"
 #import "TipsTabViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UIImageView *trashImageView;
 
 @end
 
-const NSUInteger iconNum = 8;
-const CGFloat iconHeight = 60.0;
-const CGFloat iconWidth = 60.0;
+const NSUInteger iconNum = 7;
+const CGFloat iconHeight = 72.0;
+const CGFloat iconWidth = 72.0;
+const CGFloat iconMargin = 20.0;
 
 @implementation ViewController
 
@@ -24,25 +27,27 @@ const CGFloat iconWidth = 60.0;
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self setBackgroundImage];
+    [self setTrashImage];
+    
     for (int i=0; i < iconNum; i++) {
-        NSString *imageName = [NSString stringWithFormat:@"sun"];
+        NSString *imageName = [NSString stringWithFormat:@"Image-%d", i];
         UIImage *img = [UIImage imageNamed:imageName];
-        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 60, 60)];
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, iconWidth, iconHeight)];
         [btn setBackgroundImage:img forState:UIControlStateNormal];
-        [btn addTarget:self action:@selector(hoge:) forControlEvents:UIControlEventTouchUpInside];
+        [btn addTarget:self action:@selector(nextPage:) forControlEvents:UIControlEventTouchUpInside];
         btn.tag = i + 1;
         [_scrollView addSubview:btn];
     }
     [self setupScrollContent];
     
     // adjust the size of the navigationBar and statusBar
-    _scrollView.contentInset=UIEdgeInsetsMake(-64.0, 0.0, 0.0, 0.0);
-    _scrollView.scrollIndicatorInsets=UIEdgeInsetsMake(-64.0,0.0, 0.0,0.0);
-    
-    // navigationControllerを透明にする
-    [self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-    self.navigationController.navigationBar.shadowImage = [UIImage new];
-    self.navigationController.navigationBar.translucent = YES;
+    _scrollView.contentInset=UIEdgeInsetsMake(-20.0, 0.0, 0.0, 0.0);
+    _scrollView.scrollIndicatorInsets=UIEdgeInsetsMake(-20.0,0.0, 0.0,0.0);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,29 +56,101 @@ const CGFloat iconWidth = 60.0;
 }
 
 
+- (void)setBackgroundImage {
+    UIGraphicsBeginImageContext(self.view.frame.size);
+//    [[UIImage imageNamed:@"Sunny"] drawInRect:self.view.bounds];
+    [[UIImage imageNamed:@"Cloudy"] drawInRect:self.view.bounds];
+//    [[UIImage imageNamed:@"Rainy"] drawInRect:self.view.bounds];
+//    [[UIImage imageNamed:@"Snowy"] drawInRect:self.view.bounds];
+    UIImage *backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.view.backgroundColor = [UIColor colorWithPatternImage:backgroundImage];
+}
+
+- (void)setTrashImage {
+    _trashImageView.image = [UIImage imageNamed:@"Bottle"];
+}
+
+
 - (void)setupScrollContent {
     UIImageView *view = nil;
     NSArray *subviews = [_scrollView subviews];
     // 描画開始の x,y 位置
-    CGFloat px = 28.0;
+    CGFloat px = iconMargin;
     CGFloat py = 0.0;
     for (view in subviews) {
-        NSLog(@"tag = %ld",(long)view.tag);
         if ([view isKindOfClass:[UIButton class]] && view.tag > 0) {
             CGRect frame = view.frame;
             frame.origin = CGPointMake(px, py);
             view.frame = frame;
             
-            px += (iconWidth + 28);
+            px += (iconWidth + iconMargin);
         }
     }
     
     // UIScrollViewのコンテンツサイズを計算
-    [_scrollView setContentSize:CGSizeMake( iconWidth * iconNum + 28 * (iconNum + 1), iconHeight)];
+    [_scrollView setContentSize:CGSizeMake( iconWidth * iconNum + iconMargin * (iconNum + 1), iconHeight)];
 }
 
-- (void)hoge:(UIButton *)sender {
-    NSLog(@"%ld", (long)sender.tag);
+- (void)nextPage:(UIButton *)sender {
+    switch (sender.tag) {
+        case 3:{
+            TipsTabViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"TipsTabView"];
+            [self presentViewController:controller animated:YES completion:nil];
+            break;
+        }
+            
+        default:
+            NSLog(@"%ld", (long)sender.tag);
+            break;
+    }
+}
+
+
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSInteger count;
+    count = 10;
+    return count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    tableView.separatorColor = [UIColor clearColor];
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = @"hogehoge";
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%ld", (long)indexPath.row);
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // 画面遷移
+}
+
+
+#pragma mark - UISearchDisplayControllerDelegate
+- (BOOL)searchDisplayController:controller shouldReloadTableForSearchString:(NSString *)searchString {
+    [self filterContainsWithSearchText:searchString];
+    return YES;
+}
+
+- (void)filterContainsWithSearchText:(NSString *)searchText {
+    // searchTextをもとに検索する
 }
 
 @end

4631d6959346d0e184b66ea9880062d01c3c3fdd
HIRAI Tatsuhito <slateblue1121@gmail.com>
2015-08-15 22:31:17 +0900

    通知を仮実装

--- iidaPro-2015/AlarmViewController.m
+++ iidaPro-2015/AlarmViewController.m
@@ -65,12 +65,20 @@
 - (IBAction)morningAlarmOnOff:(id)sender {
   _morningAlarmActivity = [NSNumber numberWithBool: _morningAlarmSwitch.on];
   _morningAlarmLabel.enabled = _morningAlarmSwitch.on;
-  
+  if (_morningAlarmSwitch.on){
+    [self setMorningAlarm: _morningAlarmTime];
+  }else{
+    [self deleteMorningAlarm];
+  }
 }
 - (IBAction)nightAlarmOnOff:(id)sender {
   _nightAlarmActivity = [NSNumber numberWithBool: _nightAlarmSwitch.on];
   _nightAlarmLabel.enabled = _nightAlarmSwitch.on;
-
+  if (_nightAlarmSwitch.on){
+    [self setNightAlarm: _nightAlarmTime];
+  }else{
+    [self deleteNightAlarm];
+  }
 }
 
 - (IBAction)dPValueChanged:(id)sender {
@@ -82,14 +90,12 @@
     _morningAlarmLabel.text = [dateFormatter stringFromDate:_datepicker.date];
     
     [self setMorningAlarm: _datepicker.date];
-    
   }
   if ([_dPView isEqual:@"night"]) {
     _nightAlarmTime = _datepicker.date;
     _nightAlarmLabel.text = [dateFormatter stringFromDate:_datepicker.date];
     
     [self setNightAlarm: _datepicker.date];
-    
   }
 }
 
@@ -107,16 +113,58 @@
 }
 
 - (void)setMorningAlarm:(NSDate*)date {
+  
+  if (date != nil && [date timeIntervalSinceNow] >0){
+  
+  //朝の通知がすでにある場合、削除する
+  [self deleteMorningAlarm];
+  
+  
   //ここで朝の通知を設定
+  UILocalNotification *morningNote = [[UILocalNotification alloc] init];
+  morningNote.fireDate = date;
+  [morningNote.userInfo setValue:@"morning" forKey:@"id"];
+  morningNote.alertBody = @"朝のアラーム";
+  
+  }
+  
 }
 - (void)deleteMorningAlarm{
   //朝の通知を消す設定。スイッチがOnOffされた時など用
+  
+  for(UILocalNotification *notification in [[UIApplication sharedApplication] scheduledLocalNotifications]) {
+    if([[notification.userInfo objectForKey:@"id"] isEqual: @"morning"]) {
+      [[UIApplication sharedApplication] cancelLocalNotification:notification];
+    }
+  }
+  
 }
 
 - (void)setNightAlarm:(NSDate*)date {
   //ここで夜の通知を設定
+  if (date != nil && [date timeIntervalSinceNow] >0){
+  
+  [self deleteNightAlarm];
+  
+  UILocalNotification *nightNote = [[UILocalNotification alloc] init];
+  nightNote.fireDate = date;
+  [nightNote.userInfo setValue:@"night" forKey:@"id"];
+  nightNote.alertBody = @"夜のアラーム";
+
+  }
 }
 - (void)deleteNightAlarm{
   //夜の通知を消す設定。スイッチがOnOffされた時など用
+  
+  for(UILocalNotification *notification in [[UIApplication sharedApplication] scheduledLocalNotifications]) {
+    if([[notification.userInfo objectForKey:@"id"] isEqual: @"night"]) {
+      [[UIApplication sharedApplication] cancelLocalNotification:notification];
+    }
+  }
+
 }
+
+
+
+
 @end

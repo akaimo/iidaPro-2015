247bb8307c8dcfb4252dfaab703fd582354aacab
shuhei <kawashu0723@gmail.com>
2015-08-18 18:13:23 +0900

    CocoaPods install Realm

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+
+target 'iidaPro-2015' do
+    pod 'Realm'
+end
+
+target 'iidaPro-2015Tests' do
+    pod 'Realm/Headers'
+end
---
+++ Pods/Target Support Files/Pods-iidaPro-2015/Pods-iidaPro-2015-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iidaPro_2015 : NSObject
+@end
+@implementation PodsDummy_Pods_iidaPro_2015
+@end

ab588b73cc029e6ca61eaa17008a3ddc9cc515da
shuhei <kawashu0723@gmail.com>
2015-09-29 22:52:11 +0900

    fix menu action

--- iidaPro-2015/ViewController.m
+++ iidaPro-2015/ViewController.m
@@ -261,7 +261,7 @@ const CGFloat iconMargin = 20.0;
 #pragma mark -
 - (void)nextPage:(UIButton *)sender {
     switch (sender.tag) {
-        case 3:{
+        case 1:{
             TipsTabViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"TipsTabView"];
             [self presentViewController:controller animated:YES completion:nil];
             break;

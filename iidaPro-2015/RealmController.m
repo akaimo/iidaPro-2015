9a9974f3e19084f730388f39363c9a2ff5279d4a
shuhei <kawashu0723@gmail.com>
2015-10-16 18:55:42 +0900

    add json data for realm

--- iidaPro-2015/RealmController.m
+++ iidaPro-2015/RealmController.m
@@ -15,74 +15,30 @@
 @implementation RealmController
 
 - (void)createTestTable {
-    RLMRealm *realm;
+//    RLMRealm *realm;
 //    NSLog(@"Realm: %@", [RLMRealmConfiguration defaultConfiguration].path);
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     [manager GET:@"http://153.120.170.41:3000/api/v1/trash" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        NSLog(@"JSON: %@", responseObject);
+//        NSLog(@"JSON: %@", responseObject);
+        NSLog(@"Realm更新開始");
+        for (int i=0; i<[responseObject count]; i++) {
+            Classification *classifi = [[Classification alloc] init];
+            classifi.num = i;
+            classifi.title = [responseObject[i] valueForKey:@"title"];
+            classifi.read = [responseObject[i] valueForKey:@"read"];
+            classifi.classification = @"燃えるごみ";
+            classifi.knowledge = nil;
+            
+            RLMRealm *realm = [RLMRealm defaultRealm];
+            [realm beginWriteTransaction];
+            [Classification createOrUpdateInRealm:realm withValue:classifi];
+            [realm commitWriteTransaction];
+        }
+        NSLog(@"Realm更新完了");
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
-    
-    for (int i=0; i<20; i++) {
-        // 豆知識
-        Knowledge *knowledge = [[Knowledge alloc] init];
-        knowledge.num = i;
-        knowledge.title = @"huga";
-        knowledge.detail = @"hugahuga";
-        
-        // 分別辞典
-        Classification *classifi = [[Classification alloc] init];
-        classifi.num = i;
-        if (i == 0) {
-            classifi.title = @"トレー";
-            classifi.read = @"とれー";
-            classifi.classification = @"プラスチック製容器包装";
-            classifi.knowledge = knowledge;
-        } else if (i == 1) {
-            classifi.title = @"ビン";
-            classifi.read = @"びん";
-            classifi.classification = @"空きびん";
-            classifi.knowledge = nil;
-        } else if (i == 2) {
-            classifi.title = @"缶";
-            classifi.read = @"かん";
-            classifi.classification = @"空き缶";
-            classifi.knowledge = nil;
-        } else if (i == 3) {
-            classifi.title = @"ああ";
-            classifi.read = @"ああ";
-            classifi.classification = @"燃えるごみ";
-            classifi.knowledge = nil;
-        } else if (i == 4) {
-            classifi.title = @"あい";
-            classifi.read = @"あい";
-            classifi.classification = @"燃えるごみ";
-            classifi.knowledge = nil;
-        } else if (i == 5) {
-            classifi.title = @"うい";
-            classifi.read = @"うい";
-            classifi.classification = @"燃えるごみ";
-            classifi.knowledge = nil;
-        } else if (i == 6) {
-            classifi.title = @"いい";
-            classifi.read = @"いい";
-            classifi.classification = @"燃えるごみ";
-            classifi.knowledge = nil;
-        } else {
-            classifi.title = @"hoge";
-            classifi.read = @"ほげ";
-            classifi.classification = @"hogehoge";
-            classifi.knowledge = nil;
-        }
-        
-        realm = [RLMRealm defaultRealm];
-        [realm beginWriteTransaction];
-        [Knowledge createOrUpdateInRealm:realm withValue:knowledge];
-        [Classification createOrUpdateInRealm:realm withValue:classifi];
-        [realm commitWriteTransaction];
-    }
 }
 
 - (void)deleteTestTable {

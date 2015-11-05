74ea6f95c49903e78168c5a1361ecbf33b2bf046
shuhei <kawashu0723@gmail.com>
2015-11-05 21:40:29 +0900

    bug sharedapplication

--- iidaPro-2015/AlarmPopUpView.m
+++ iidaPro-2015/AlarmPopUpView.m
@@ -13,7 +13,7 @@
 - (id)init {
     self = [super init];
     if (self) {
-        
+        [self setup:CGRectMake(0, 0, 0, 0)];
     }
     return self;
 }
@@ -21,7 +21,7 @@
 - (id)initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        
+        [self setup:CGRectMake(0, 0, 0, 0)];
     }
     return self;
 }
@@ -29,9 +29,20 @@
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        
+        [self setup:frame];
     }
     return self;
+}
+
+- (void)setup:(CGRect)frame {
+    self.frame = frame;
+    
+    CGFloat width = frame.size.width * 0.8;
+    CGFloat wMargin = frame.size.width * 0.1;
+    CGFloat height = 300.0;
+    CGFloat hMargin = (frame.size.height - 300) / 2;
+//    self.frame = CGRectMake(wMargin, hMargin, width, height);
+    self.backgroundColor = [UIColor lightGrayColor];
 }
 
 @end

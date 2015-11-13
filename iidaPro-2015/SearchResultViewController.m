7a9e71e7fa87546e3fdacfd8752d65f1f50ce9c4
shuhei <kawashu0723@gmail.com>
2015-11-13 22:16:07 +0900

    change index color

--- iidaPro-2015/SearchResultViewController.m
+++ iidaPro-2015/SearchResultViewController.m
@@ -33,7 +33,10 @@
     [UINavigationBar appearance].barTintColor = [UIColor colorWithRed:86/255.0 green:96/255.0 blue:133/255.0 alpha:1.000];
     [UINavigationBar appearance].tintColor = [UIColor whiteColor];
     [UINavigationBar appearance].titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
+    
     _searchTableView.backgroundColor = [UIColor clearColor];
+    _searchTableView.sectionIndexColor = [UIColor whiteColor];
+    _searchTableView.sectionIndexBackgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.1];
     
     _searchBar.placeholder = @"Search";
     
@@ -107,7 +110,7 @@
 
 - (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.bounds.size.width, 40)];
-    headerView.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.4];
+    headerView.backgroundColor = [UIColor colorWithRed:41/255.0 green:52/255.0 blue:92/255.0 alpha:0.95];
     
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, headerView.frame.size.width - 20, 22)];
     label.text = [_sectionList objectAtIndex:section];

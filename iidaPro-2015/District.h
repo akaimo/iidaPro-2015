5a7e6d804993f596ec96c3d8721aec861b798e84
shuhei <kawashu0723@gmail.com>
2015-11-14 22:45:46 +0900

    add REALM table District

---
+++ iidaPro-2015/District.h
@@ -0,0 +1,27 @@
+//
+//  District.h
+//  iidaPro-2015
+//
+//  Created by akaimo on 11/14/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface District : RLMObject
+@property NSInteger num;
+@property NSString *area;
+@property NSString *town;
+@property NSString *normal_1;
+@property NSString *normal_2;
+@property NSString *bottle;
+@property NSString *plastic;
+@property NSString *mixedPaper;
+@property NSString *bigRefuse_date;
+@property NSString *bigRefuse_1;
+@property NSString *bigRefuse_2;
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<District>
+RLM_ARRAY_TYPE(District)
---
+++ iidaPro-2015/District.m
@@ -0,0 +1,22 @@
+//
+//  District.m
+//  iidaPro-2015
+//
+//  Created by akaimo on 11/14/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+#import "District.h"
+
+@implementation District
+
++ (nullable NSString *)primaryKey {
+    return @"num";
+}
+
++ (NSArray *)requiredProperties {
+    return @[@"area", @"town", @"normal_1", @"normal_2", @"bottle", @"plasstic", @"mixedPaper",
+             @"bigRefuse_date", @"bigRefuse_1", @"bigRefuse_2"];
+}
+
+@end

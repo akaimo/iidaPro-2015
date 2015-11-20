db5aa8589ca56b51808d78f4e1bfecef9989a75f
shuhei <kawashu0723@gmail.com>
2015-11-20 18:33:35 +0900

    update db classification -> TrashCategory

---
+++ iidaPro-2015/TrashCategory.h
@@ -0,0 +1,24 @@
+//
+//  TrashCategory.h
+//  iidaPro-2015
+//
+//  Created by akaimo on 11/20/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface TrashCategory : RLMObject
+@property NSInteger num;
+@property NSString *title;
+@property NSString *read;
+@property NSString *read_head;
+@property NSString *classification;
+@property BOOL detail;
+@property NSString *category;
+@property NSString *info;
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<TrashCategory>
+RLM_ARRAY_TYPE(TrashCategory)

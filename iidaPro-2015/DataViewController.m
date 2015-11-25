46fc05b858a13361df35db435b62e4e24e3048a0
shuhei <kawashu0723@gmail.com>
2015-11-25 23:26:58 +0900

    add trash image icon

--- iidaPro-2015/DataViewController.m
+++ iidaPro-2015/DataViewController.m
@@ -8,9 +8,11 @@
 
 #import "DataViewController.h"
 #import "CalendarTableViewCell.h"
+#import "AppDelegate.h"
 
 @interface DataViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (retain, nonatomic) UITableView *calendarTableView;
+@property (retain, nonatomic) NSDictionary *areaData;
 
 @end
 
@@ -18,6 +20,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    _areaData = [NSDictionary dictionaryWithDictionary:[ud objectForKey:@"district"]];
     
     CGRect rect = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y + 66, self.view.frame.size.width, self.view.frame.size.height - 66);
     _calendarTableView = [[UITableView alloc] initWithFrame:rect];
@@ -32,6 +37,39 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UIImage *)trashImage:(NSString *)weekday {
+    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    NSString *todayCategory;
+    for (NSString *category in appDelegate.categoryArray) {
+        NSString *categoryKey = [appDelegate.categoryDict valueForKey:category];
+        if ([weekday  isEqual: [_areaData valueForKey:categoryKey]]) {
+            todayCategory = categoryKey;
+            break;
+        }
+    }
+    
+    UIImage *categoryImage;
+    if (![todayCategory  isEqual: @""] && [weekday  isEqual: [_areaData valueForKey:@"bigRefuse_date"]]) {
+        // TODO: 小物金属とその他のごみがかぶっている
+    } else {
+        if ([todayCategory  isEqual:@"normal_1"] || [todayCategory isEqual:@"normal_2"]) {
+            categoryImage = [UIImage imageNamed:@"S_Normal"];
+        } else if ([todayCategory isEqual:@"bottle"]) {
+            categoryImage = [UIImage imageNamed:@"S_Can"];
+        } else if ([todayCategory isEqual:@"plastic"]) {
+            categoryImage = [UIImage imageNamed:@"S_plastic"];
+        } else if ([todayCategory isEqual:@"mixedPaper"]) {
+            categoryImage = [UIImage imageNamed:@"S_Mixed"];
+        } else if ([todayCategory isEqual:@"bigRefuse"]) {
+            // TODO: 隔週のチェックをする
+            categoryImage = [UIImage imageNamed:@"S_BigRefuse"];
+        }
+    }
+    
+    return categoryImage;
 }
 
 
@@ -53,6 +91,10 @@
     df.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en"];
     NSString* weekDayStr = df.weekdaySymbols[comps.weekday-1];
     cell.weekdayLabel.text = weekDayStr;
+    
+    df.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ja"];
+    weekDayStr = df.shortWeekdaySymbols[comps.weekday-1];
+    cell.iconImageView.image = [self trashImage:weekDayStr];
     
     comps = [calendar components:NSCalendarUnitDay fromDate:date];
     NSString *dayStr = [NSString stringWithFormat:@"%ld", (long)comps.day];

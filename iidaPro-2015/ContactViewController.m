2c7049bba11c9f29403cb90fa4d8356845a7d896
shuhei <kawashu0723@gmail.com>
2015-12-08 22:09:30 +0900

    auto change contact office from district data

--- iidaPro-2015/ContactViewController.m
+++ iidaPro-2015/ContactViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UITextView *secondTextView;
 @property (weak, nonatomic) IBOutlet UILabel *therdLabel;
 @property (weak, nonatomic) IBOutlet UITextView *therdTextView;
+@property (retain, nonatomic) NSDictionary *areaData;
 
 @end
 
@@ -24,6 +25,8 @@
     [super viewDidLoad];
     
     self.title = @"問い合わせ";
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    _areaData = [NSDictionary dictionaryWithDictionary:[ud objectForKey:@"district"]];
     
     self.view.layer.contents = (id)[UIImage imageNamed:@"Base"].CGImage;
     [UINavigationBar appearance].barTintColor = [UIColor colorWithRed:86/255.0 green:96/255.0 blue:133/255.0 alpha:1.000];
@@ -47,8 +50,19 @@
     _firstLabel.textColor = [UIColor whiteColor];
     _firstLabel.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.1];
     
-    // TODO: 登録された区によって切り替える
-    _firstTextView.text = @"川崎市環境事業所 044-541-2043";
+    NSString *office;
+    if ([[_areaData valueForKey:@"office"]  isEqual: @"南部"]) {
+        office = @"南部生活環境事業所 044-266-5747";
+    } else if ([[_areaData valueForKey:@"office"]  isEqual: @"川崎"]) {
+        office = @"川崎生活環境事業所 044-541-2043";
+    } else if ([[_areaData valueForKey:@"office"]  isEqual: @"中原"]) {
+        office = @"中原生活環境事業所 044-411-9220";
+    } else if ([[_areaData valueForKey:@"office"]  isEqual: @"宮前"]) {
+        office = @"宮前生活環境事業所 044-866-9131";
+    } else if ([[_areaData valueForKey:@"office"]  isEqual: @"多摩"]) {
+        office = @"多摩生活環境事業所 044-933-4111";
+    }
+    _firstTextView.text = office;
     _firstTextView.textColor = [UIColor whiteColor];
     _firstTextView.backgroundColor = [UIColor clearColor];
     _firstTextView.editable = NO;

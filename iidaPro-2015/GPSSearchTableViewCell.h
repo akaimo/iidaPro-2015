0453b66c373f5fe234776f83c4fc342a8de0ba87
shuhei <kawashu0723@gmail.com>
2015-12-11 17:44:00 +0900

    add gps search button

---
+++ iidaPro-2015/GPSSearchTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  GPSSearchTableViewCell.h
+//  iidaPro-2015
+//
+//  Created by akaimo on 12/11/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPSSearchTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *searchBtn;
+
+@end
---
+++ iidaPro-2015/GPSSearchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GPSSearchTableViewCell.m
+//  iidaPro-2015
+//
+//  Created by akaimo on 12/11/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+#import "GPSSearchTableViewCell.h"
+
+@implementation GPSSearchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

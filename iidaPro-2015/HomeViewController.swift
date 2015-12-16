69746691a27e70936682676e1e5424cb006589c2
shuhei <kawashu0723@gmail.com>
2015-12-16 22:31:55 +0900

    add eventLabel

--- iidaPro-2015/HomeViewController.swift
+++ iidaPro-2015/HomeViewController.swift
@@ -14,6 +14,7 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     @IBOutlet weak var trashImageView: UIImageView!
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var eventLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()

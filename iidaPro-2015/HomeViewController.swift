19e6569d572112c73b59ebdbb84306c355ef2523
shuhei <kawashu0723@gmail.com>
2015-12-18 23:01:46 +0900

    fix trash image view point

--- iidaPro-2015/HomeViewController.swift
+++ iidaPro-2015/HomeViewController.swift
@@ -83,7 +83,7 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     private func fetchWeatherThema() {
         // TODO: 天気APIから取得
-        self.weatherThema = Weather.Sunny
+        self.weatherThema = Weather.Snowy
     }
     
     private func updateAreaData() {

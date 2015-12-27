d1b0be1f5f4ab206109a8e88dc942b3b0f7b5263
shuhei <kawashu0723@gmail.com>
2015-12-27 16:07:45 +0900

    add calendar cell

---
+++ iidaPro-2015/CalendarCell.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarCell.swift
+//  iidaPro-2015
+//
+//  Created by akaimo on 12/27/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: UITableViewCell {
+    @IBOutlet weak var weekdayLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var alarmButton: UIButton!
+    @IBOutlet weak var icon1ImageView: UIImageView!
+    @IBOutlet weak var icon2ImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

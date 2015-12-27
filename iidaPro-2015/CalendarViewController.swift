a1b7867a4184a18a2275d7828fc08fe8d04d6b24
shuhei <kawashu0723@gmail.com>
2015-12-27 15:43:07 +0900

    add calendarVC.swift

---
+++ iidaPro-2015/CalendarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CalendarViewController.swift
+//  iidaPro-2015
+//
+//  Created by akaimo on 12/27/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    @IBOutlet weak var calendarTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

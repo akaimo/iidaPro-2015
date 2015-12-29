b05a7187335feb84d9e9fc57d4acaba6e495b692
shuhei <kawashu0723@gmail.com>
2015-12-29 17:04:59 +0900

    set tableview sectionheader color

--- iidaPro-2015/CalendarViewController.swift
+++ iidaPro-2015/CalendarViewController.swift
@@ -47,7 +47,32 @@ class CalendarViewController: UIViewController, UITableViewDelegate {
     
     // MARK: - UITableViewDelefate
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath == self.calendarModel.todayIndexPath {
+            return 110.0
+        }
         return 55.0
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20.0
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRectMake(0, 0, tableView.bounds.size.width, 20))
+        headerView.backgroundColor = UIColor(red: 41/255.0, green: 52/255.0, blue: 92/255.0, alpha: 0.95)
+        
+        let label = UILabel(frame: CGRectMake(20, 0, headerView.frame.size.width - 20, headerView.frame.size.height))
+        label.text = self.calendarModel.monthStrArray[section]
+        label.font = UIFont.boldSystemFontOfSize(16.0)
+        label.shadowOffset = CGSizeMake(0, 1)
+        label.shadowColor = UIColor.grayColor()
+        label.backgroundColor = UIColor.clearColor()
+        label.textColor = UIColor.whiteColor()
+        
+        headerView.addSubview(label)
+        tableView.sectionHeaderHeight = headerView.frame.size.height
+        
+        return headerView
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {

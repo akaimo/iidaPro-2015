e7a1b9ff7c5871d71b790ec6ef41aa177b176a22
shuhei <kawashu0723@gmail.com>
2015-12-31 15:01:05 +0900

    add contactVC.swift

---
+++ iidaPro-2015/ContactViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ContactViewController.swift
+//  iidaPro-2015
+//
+//  Created by akaimo on 12/31/15.
+//  Copyright © 2015 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstTextView: UITextView!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var secondTextView: UITextView!
+    @IBOutlet weak var thirdLabel: UILabel!
+    @IBOutlet weak var thirdTextView: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

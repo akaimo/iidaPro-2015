d9a9310d5865c7ffa7579be9dcd7524051b28b46
shuhei <kawashu0723@gmail.com>
2016-01-03 21:49:15 +0900

    add settingVC.swift

---
+++ iidaPro-2015/SettingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingViewController.swift
+//  iidaPro-2015
+//
+//  Created by akaimo on 1/3/16.
+//  Copyright © 2016 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    @IBOutlet weak var settingTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
